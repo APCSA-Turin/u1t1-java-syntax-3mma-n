6a579820fc99154e1a6ba69fad3d3544b2112a25
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-09-09 16:04:39 +0000

    9/9

---
+++ EmmaCode.java
@@ -0,0 +1,16 @@
+public class EmmaCode {
+    public static void main(String[] args) {
+        System.out.print("Are you ready for a REALLLLY hard math problem? ");
+        System.out.println("You're not?");
+        System.out.println("That's a shame...");
+        System.out.println();
+        System.out.println("Because I'm gonna show you anyway!");
+        System.out.print("What is 2 + 0 * 5? ");
+        System.out.println("Please come up with an answer before continuing.");
+        System.out.println("\n \n \n");
+        System.out.print("That's right, it's ");
+        System.out.print(2 + (0 * 5));
+        System.out.println("!!! It's important to remember the order of operations :D");
+        System.out.println("SThanks for participating!");
+    }
+}
