9e0f9d651ccd58b5b6fb110c71d307511bbdd6b4
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-09-09 16:30:40 +0000

    9/9 v2

--- EmmaCode.java
+++ EmmaCode.java
@@ -1,5 +1,9 @@
 public class EmmaCode {
     public static void main(String[] args) {
+        /*
+        I made this program to simulate a little math problem
+        It doesn't take input but simply asks the user to solve it before looking at the answer
+         */
         System.out.print("Are you ready for a REALLLLY hard math problem? ");
         System.out.println("You're not?");
         System.out.println("That's a shame...");
@@ -9,7 +13,7 @@ public class EmmaCode {
         System.out.println("Please come up with an answer before continuing.");
         System.out.println("\n \n \n");
         System.out.print("That's right, it's ");
-        System.out.print(2 + (0 * 5));
+        System.out.print(2 + (0 * 5)); // Solves problem, 0 * 5 = 0, + 2 = 2.
         System.out.println("!!! It's important to remember the order of operations :D");
         System.out.println("SThanks for participating!");
     }
